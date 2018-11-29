8a138f268dad6f417d4a924b95b6a4da8aae0fe7
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-11-29 07:53:38 +0000

    [+] Drawing primitives

--- demo.cpp
+++ demo.cpp
@@ -68,19 +68,45 @@ IplImage *convertImage(IplImage *img ){
 
 }
 
-int main ( int argn, char *argv[]  ) {
-    std::cout << std::endl << "Hi this is " << __FILE__ << "." << std::endl;
+void drawLine(IplImage *img, int maxValue){
+    const CvScalar colorLine = CV_RGB(0,0,maxValue);
+    const int thicknessLine  = 3;
+    const CvPoint p0 = cvPoint(0,0);
+    const CvPoint p1 = cvPoint(img->width,img->height);
+    cvLine( img, p0, p1, colorLine, thicknessLine);
+
+}
+
+IplImage *createSimpleImage(const int width, const int height){
+    // create a colour change
+    const int nChannels = 3;
+    const int depth = IPL_DEPTH_8U;
+    const int maxValue = 255; // depends on depth : IPL Depth 8u -> 255
+    IplImage *img = cvCreateImage(cvSize(width,height),depth,nChannels);
+    const int shorterEdge = MIN(width, height);
+
+    drawLine(img, maxValue);
+    // const CvScalar colorLine = CV_RGB(0,0,maxValue);
+    // const int thicknessLine  = 3;
+    // const CvPoint p0 = cvPoint(0,0);
+    // const CvPoint p1 = cvPoint(img->width,img->height);
+    // cvLine( img, p0, p1, colorLine, thicknessLine);
+
+    return img;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << std::endl << "Hi! this is" << __FILE__ << "."  << std::endl ;
+
+    //create image
+    IplImage *img = createSimpleImage( 640, 480);
+
+    // Create and show image
     const char *window = "image";
-    const char *fileImg = getFilename(argn,argv,1,"../data/test.png");
-
-    IplImage *img = loadAndShowImage(fileImg,CV_LOAD_IMAGE_UNCHANGED,window,0,0);
-    // IplImage *img = loadAndShowImage(fileImg,CV_LOAD_IMAGE_GRAYSCALE,window,0,0);
-
-
-    std::cout << "read in image from: " << fileImg << std::endl;
-
-
-    IplImage *imgGray = convertImage( img );
+    cvNamedWindow(window, CV_WINDOW_AUTOSIZE);
+    cvMoveWindow(window, 0, 0);
+    cvShowImage(window, img);
 
     std::cout << "press any key while image window has focus." << std::endl;
     const int durationInMilliSeconds = 0;
@@ -89,6 +115,7 @@ int main ( int argn, char *argv[]  ) {
 
 
     saveModImage("test",img);
+    // convertImage(img);
 
     //release memory
     cvReleaseImage(&img);
@@ -97,8 +124,43 @@ int main ( int argn, char *argv[]  ) {
     cvDestroyAllWindows();
 
     std::cout << "completed" << __FILE__ << std::endl;
+
     return 0;
- }
+}
+
+
+
+// int main ( int argn, char *argv[]  ) {
+//     std::cout << std::endl << "Hi this is " << __FILE__ << "." << std::endl;
+//     const char *window = "image";
+//     const char *fileImg = getFilename(argn,argv,1,"../data/test.png");
+
+//     IplImage *img = loadAndShowImage(fileImg,CV_LOAD_IMAGE_UNCHANGED,window,0,0);
+//     // IplImage *img = loadAndShowImage(fileImg,CV_LOAD_IMAGE_GRAYSCALE,window,0,0);
+
+
+//     std::cout << "read in image from: " << fileImg << std::endl;
+
+
+//     IplImage *imgGray = convertImage( img );
+
+//     std::cout << "press any key while image window has focus." << std::endl;
+//     const int durationInMilliSeconds = 0;
+//     int key = cvWaitKey( durationInMilliSeconds );
+//     std::cout << "received key code " << key << std::endl;
+
+
+//     saveModImage("test",img);
+
+//     //release memory
+//     cvReleaseImage(&img);
+//     // cvReleaseImage(&imgGray);
+//     cvDestroyWindow(window);
+//     cvDestroyAllWindows();
+
+//     std::cout << "completed" << __FILE__ << std::endl;
+//     return 0;
+//  }
 
 
 // int main()
